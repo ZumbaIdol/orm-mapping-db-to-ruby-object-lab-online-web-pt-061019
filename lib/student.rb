a997289724558487121d450da4c2353b707ae43e
David Foote <david.a.foote@gmail.com>
2019-08-21 03:07:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -114,7 +114,7 @@ def self.students_below_12th_grade
       SELECT *
       FROM students
       WHERE grade = 10
-      
+      LIMIT ?
     SQL
     DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
