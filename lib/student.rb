0b789d786362c2aa8757f54d153a19100f48261a
David Foote <david.a.foote@gmail.com>
2019-08-21 03:17:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,7 +85,7 @@ def self.students_below_12th_grade
     end
   end
   
-  def self.first_x_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT *
       FROM students
